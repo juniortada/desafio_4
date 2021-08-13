0df3f9816c80df0607a8f6fed9a7f7d433be1034
juniortada <juniortada@gmail.com>
2021-08-13 19:33:34 -0300

    [new] add celsius temperature in view #1

--- weather-challenge/app/controllers/dashboard_controller.rb
+++ weather-challenge/app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @weather = Openweather2.get_weather(city: 'Sao Paulo,BR', units: 'imperial')
+    @weather_celsius = WeatherCelsius.new
   end
 end
