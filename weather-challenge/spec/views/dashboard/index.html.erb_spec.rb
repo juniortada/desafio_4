1d25b8a6bfb17201da59fb2af936b4dc1d3bc12d
juniortada <juniortada@gmail.com>
2021-08-13 19:35:39 -0300

    [fix] clean spec tests

--- weather-challenge/spec/views/dashboard/index.html.erb_spec.rb
+++ weather-challenge/spec/views/dashboard/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "dashboard/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe 'dashboard/index.html.erb', type: :view do
+#   before(:each) do 
+#     assign(:weather_celsius, [ WeatherCelsius.create!(temperature: 15.25) ])
+#   end 
+#   it 'display celsius weathers' do
+#     render
+#     expect(rendered).to match /15.25/
+#   end
+# end
