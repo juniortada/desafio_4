d10192463d622e24d9193540b102d8321a45ead2
juniortada <juniortada@gmail.com>
2021-08-15 19:01:11 -0300

    [new] add basic search city #2

--- weather-challenge/app/assets/javascripts/application.js
+++ weather-challenge/app/assets/javascripts/application.js
@@ -12,3 +12,4 @@
 //
 //= require popper
 //= require bootstrap-sprockets
+//= require_tree .
--- weather-challenge/app/controllers/dashboard_controller.rb
+++ weather-challenge/app/controllers/dashboard_controller.rb
@@ -1,6 +1,20 @@
 class DashboardController < ApplicationController
+  include ApiHelper
+
   def index
-    @weather = Openweather2.get_weather(city: 'Sao Paulo,BR', units: 'imperial')
     @weather_celsius = WeatherCelsius.new
+    if request.post?
+      city = params[:city]
+      begin 
+        @weather = ApiHelper.get_weather(city)
+      rescue
+        # error, default city
+        @weather = ApiHelper.get_weather("Sao Paulo")
+      ensure
+        render 'index'
+      end
+    else
+      @weather = ApiHelper.get_weather("Sao Paulo")
+    end
   end
 end
